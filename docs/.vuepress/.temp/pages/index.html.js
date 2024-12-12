7caf1a2731c987914817d4f68e0add144d2d9b69
wkdzhangting <11590395+wkdzhangting@user.noreply.gitee.com>
2024-12-12 21:33:45 +0800

    feat: 构建

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/AAATool/静态博客/myblog/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"gello\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"hello\",\"slug\":\"hello\",\"link\":\"#hello\",\"children\":[]}],\"git\":{\"updatedTime\":1734005430000,\"contributors\":[{\"name\":\"wkdzhangting\",\"username\":\"wkdzhangting\",\"email\":\"11590395+wkdzhangting@user.noreply.gitee.com\",\"commits\":2,\"url\":\"https://github.com/wkdzhangting\"}]},\"filePathRelative\":\"README.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"gello\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"hello\",\"slug\":\"hello\",\"link\":\"#hello\",\"children\":[]}],\"git\":{\"updatedTime\":1734009863000,\"contributors\":[{\"name\":\"wkdzhangting\",\"username\":\"wkdzhangting\",\"email\":\"11590395+wkdzhangting@user.noreply.gitee.com\",\"commits\":3,\"url\":\"https://github.com/wkdzhangting\"}]},\"filePathRelative\":\"README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/test/index.html.js
+++ docs/.vuepress/.temp/pages/test/index.html.js
@@ -1,16 +1,3 @@
 import comp from "D:/AAATool/静态博客/myblog/docs/.vuepress/.temp/pages/test/index.html.vue"
-const data = JSON.parse("{\"path\":\"/test/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"hello testtest\",\"slug\":\"hello-testtest\",\"link\":\"#hello-testtest\",\"children\":[]},{\"level\":3,\"title\":\"h\",\"slug\":\"h\",\"link\":\"#h\",\"children\":[]}],\"git\":{},\"filePathRelative\":\"test/README.md\"}")
+const data = JSON.parse("{\"path\":\"/test/\",\"title\":\"\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"hello testtest\",\"slug\":\"hello-testtest\",\"link\":\"#hello-testtest\",\"children\":[]},{\"level\":3,\"title\":\"h\",\"slug\":\"h\",\"link\":\"#h\",\"children\":[]}],\"git\":{\"updatedTime\":1734009863000,\"contributors\":[{\"name\":\"wkdzhangting\",\"username\":\"wkdzhangting\",\"email\":\"11590395+wkdzhangting@user.noreply.gitee.com\",\"commits\":1,\"url\":\"https://github.com/wkdzhangting\"}]},\"filePathRelative\":\"test/README.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
